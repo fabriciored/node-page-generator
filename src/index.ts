e6c0d739db4c39d87513da94da3e8a51c1816059
fabriciored <fabriciojlsantos@gmail.com>
2022-02-08 00:52:06 -0300

    create page services

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
 import express, { Application, Request, Response } from 'express'
 
+import { generatePage } from './services/page/generatePage'
+
 const app: Application = express()
 const port = 3000
+const router = express.Router()
+
+app.use(express.json({
+  type: '*/*'
+}))
 
 app.get('/', (request: Request, response: Response) => {
   response.send(200)
 })
 
+router.post('/generate', function (request: Request, response: Response) {
+  const values = Object.values(request.body)
+  generatePage(request.body)
+  console.log('created with values: ', values)
+  response.sendStatus(200)
+})
+
+app.use('/', router)
 app.listen(port)
 console.log(`Server runs on port ${port}`)
---
+++ src/services/page/pageProps.ts
@@ -0,0 +1,17 @@
+export interface pageProperties {
+    navbar: {
+        height: string,
+        backgroundColor: string
+    },
+    pageBackgroundColor: string,
+    fontColor: string,
+    title: string,
+    logoUrl: string,
+    cards: {
+        card: {
+            imgUrl: string,
+            title: string,
+            description: string
+        }
+    }
+}
