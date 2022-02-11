9d3f56faccfbf79186ac4afd8486ff083a6c6663
fabriciored <fabriciojlsantos@gmail.com>
2022-02-11 02:13:35 -0300

    update post request response

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,6 @@ router.post('/pageinfo', function (request: Request, response: Response, fs) {
   const values = Object.values(request.body)
   html = generateHtmlString(request.body)
   console.log('created with values: ', values)
-  response.sendStatus(200)
   response.send(`
   Page created with properties: ${values}
   `)
